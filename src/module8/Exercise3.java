272b149323390f232db878c26862526e860db1ef
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-07 18:33:42 -0300

    Fix bug in login functionality

---
+++ src/module8/Exercise3.java
@@ -0,0 +1,26 @@
+package module8;
+import java.util.Scanner;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		Scanner inputValues = new Scanner(System.in);
+		float grossSalary, nightShiftAllowance, overtime, deductions, netSalary;
+		 System.out.println("Qual teu salario bruto mesmo ?");
+		 grossSalary = inputValues.nextFloat();
+		 System.out.println("Quanto de hora extra ?");
+		 overtime = inputValues.nextFloat();
+		 System.out.println("e o adcional noturno?");
+		 nightShiftAllowance = inputValues.nextFloat();
+		 System.out.println("Qual teu salario bruto mesmo ?");
+		 deductions = inputValues.nextFloat();
+		 System.out.println("ta, e qual o desconto?");
+		 netSalary = (overtime + grossSalary + nightShiftAllowance) - deductions;
+		 
+		 System.out.printf("voce ira receber %.2f de salario", netSalary);
+
+
+
+	}
+
+}
