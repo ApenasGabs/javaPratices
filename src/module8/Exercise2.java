c8df4d80b90c06e256b6fc3ccf0bb8b548d95159
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-07 18:33:20 -0300

    Add Exercise2.java with code to calculate final average

---
+++ src/module8/Exercise2.java
@@ -0,0 +1,22 @@
+package module8;
+import java.util.Scanner;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		Scanner inputValues = new Scanner(System.in);
+		float firstValue, secondValue, thirdValue, fourthValue, finalMedia;
+		System.out.println("Digite a primeira nota");
+		firstValue = inputValues.nextFloat();
+		System.out.println("Digite a segunda nota");
+		secondValue = inputValues.nextFloat();
+		System.out.println("Digite a terceira nota");
+		thirdValue = inputValues.nextFloat();
+		System.out.println("Digite a segunda nota");
+		fourthValue = inputValues.nextFloat();
+
+		finalMedia = (firstValue+ secondValue + thirdValue +fourthValue)/ 4;
+		System.out.println(finalMedia);
+	}
+
+}
