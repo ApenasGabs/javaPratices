1594aa6c57718c6e55797f0bcb70c62dae341fcf
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-07 18:35:38 -0300

    Refactor salary calculation in Exercise3.java

--- src/module8/Exercise3.java
+++ src/module8/Exercise3.java
@@ -6,19 +6,22 @@ public class Exercise3 {
 	public static void main(String[] args) {
 		Scanner inputValues = new Scanner(System.in);
 		float grossSalary, nightShiftAllowance, overtime, deductions, netSalary;
+		
 		 System.out.println("Qual teu salario bruto mesmo ?");
 		 grossSalary = inputValues.nextFloat();
+		 
 		 System.out.println("Quanto de hora extra ?");
 		 overtime = inputValues.nextFloat();
+		 
 		 System.out.println("e o adcional noturno?");
 		 nightShiftAllowance = inputValues.nextFloat();
-		 System.out.println("Qual teu salario bruto mesmo ?");
-		 deductions = inputValues.nextFloat();
+		 
 		 System.out.println("ta, e qual o desconto?");
+		 deductions = inputValues.nextFloat();
+		 
 		 netSalary = (overtime + grossSalary + nightShiftAllowance) - deductions;
 		 
 		 System.out.printf("voce ira receber %.2f de salario", netSalary);
-
 
 
 	}
