67ea3ee1205756412dcedca37f39c70c8993b2d2
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-08 15:43:06 -0300

    Add Exercise1.java

---
+++ src/module9/Exercise1.java
@@ -0,0 +1,28 @@
+package module9;
+import java.util.Scanner;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		 Scanner inputValues = new Scanner(System.in);
+		 int firstValue, secondValue, thirdValue;
+		 System.out.println("Digite o primeiro valor ");
+		 firstValue = inputValues.nextInt();
+		 System.out.println("Digite o segundo valor");
+		 secondValue = inputValues.nextInt();		 
+		 System.out.println("Digite o terceiro valor");
+		 thirdValue = inputValues.nextInt();
+		 
+		 if (firstValue + secondValue > thirdValue){
+			System.out.println("A Soma de A + B é Maior do que " + thirdValue);
+		 }
+		 if (firstValue + secondValue < thirdValue) {
+			System.out.println("A Soma de A + B é menor do que " + thirdValue);
+		 }
+		 if (firstValue + secondValue == thirdValue) {
+			System.out.println("A Soma de A + B é igual a: " + thirdValue);
+		 }
+		 
+	}
+
+}
