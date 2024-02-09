0bb1a9cdbcd3081efe28e829f00b0134177aafec
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-09 15:40:25 -0300

    Add Exercise1.java

---
+++ src/module10/Exercise1.java
@@ -0,0 +1,29 @@
+package module10;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+	// Escreva um algoritmo em Java, que leia 2 números inteiros via teclado,
+	// onde o primeiro número deve ser menor do que o segundo número. Caso
+	// contrário,
+	// deve ser exibida uma mensagem na tela informando
+	// que o intervalo é inválido e sair do programa.
+	// No intervalo informado, mostre na tela todes os números que são múltiplos
+	// de 3 e 5. Veja os exemplos abaixo:
+
+	public static void main(String[] args) {
+		Scanner inputValues = new Scanner(System.in);
+		int firstValue, secondValue;
+		boolean invalidValue;
+		
+		System.out.println("Digite o primeiro número do intervalo: 10");
+		firstValue = inputValues.nextInt();
+		System.out.println("Digite o último número do intervalo: 100");
+		secondValue = inputValues.nextInt();
+		invalidValue = firstValue > secondValue;
+		if (invalidValue) {
+			System.out.println("");
+		}
+		
+	}
+}
