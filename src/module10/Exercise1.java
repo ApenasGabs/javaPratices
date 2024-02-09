022421d863a4abcfbcd92281afbaed4f43869bee
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-09 16:07:00 -0300

    Add validation for invalid interval and print multiples of 3 and 5

--- src/module10/Exercise1.java
+++ src/module10/Exercise1.java
@@ -15,15 +15,20 @@ public class Exercise1 {
 		Scanner inputValues = new Scanner(System.in);
 		int firstValue, secondValue;
 		boolean invalidValue;
-		
+
 		System.out.println("Digite o primeiro número do intervalo: 10");
 		firstValue = inputValues.nextInt();
 		System.out.println("Digite o último número do intervalo: 100");
 		secondValue = inputValues.nextInt();
 		invalidValue = firstValue > secondValue;
 		if (invalidValue) {
-			System.out.println("");
+			System.out.println("Intervalo inválido!");
 		}
-		
+		for (int i = firstValue; i < secondValue; i++) {
+			if (i % 5 == 0 && i % 3 == 0) {
+				System.out.println(i + "é múltiplo de 3 e 5");
+			}
+		}
+		inputValues.close();
 	}
 }
