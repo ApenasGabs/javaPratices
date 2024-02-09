8d20eacda58f9a1120da16bb01a52220bbb79206
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-09 17:46:01 -0300

    Add Exercise5.java with code to calculate sum of positive numbers

---
+++ src/module10/Exercise5.java
@@ -0,0 +1,23 @@
+package module10;
+
+import java.util.Scanner;
+
+public class Exercise5 {
+  //Escreva um algoritmo em Java, que leia números inteiros via teclado, até que o número zero seja digitado.
+  //Ao final, mostre na tela a soma de todos os números digitados, que sejam positivos. Veja o exemplo abaixo:
+public static void main(String[] args) {
+		Scanner inputValues = new Scanner(System.in);
+		int typedNumber, sumOfPositiveNumbers = 0;
+    typedNumber = inputValues.nextInt();
+    
+		boolean isTheNumberValid = typedNumber != 0;
+		do {
+      System.out.println("Digite um número: ");
+      if (typedNumber%2==0) {
+        sumOfPositiveNumbers =+ typedNumber; 
+        System.out.println("A soma dos números positivos é: " + sumOfPositiveNumbers);
+      }
+    } while (isTheNumberValid);
+		inputValues.close();
+	}
+}
