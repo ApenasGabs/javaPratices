7714436716767759c3ea7564457747a45d416e07
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-14 13:52:20 -0300

    Add logic to count even and odd numbers

--- src/module11/Exercise2.java
+++ src/module11/Exercise2.java
@@ -1,10 +1,30 @@
 package module11;
 
+import java.util.Scanner;
+
 public class Exercise2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Escreva um algoritmo em Java, que leia 10 números inteiros via teclado e 
+		// mostre na tela quantos números são pares e quantos número são ímpares. 
+		Scanner inputValues = new Scanner(System.in);
+		int typedValue, oddNumbers = 0 , evenNumbers = 0;
+		
+		for(int i = 1; i<=10; i++){
+			System.out.println("Digite o " + i +"º número" );
+			typedValue = inputValues.nextInt();
 
+			if (typedValue %2 == 0 ) {
+				evenNumbers++;
+			}
+			else{
+				oddNumbers++;
+			}
+			
+		}
+		inputValues.close();
+		System.out.println("Total de números pares: " + evenNumbers);
+		System.out.println("Total de números ímpares: "+ oddNumbers);
 	}
 
 }
